d7596c843425070da75ef3fcd727026caf602899
Romplex <39164617+Romplex@users.noreply.github.com>
2020-06-23 18:46:33 +0200

    init commit

---
+++ CassandraSchwander300/src/main/java/model/CampaignDevice.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.util.UUID;
+
+public class CampaignDevice
+{
+    private UUID campaign_device_id;
+    private UUID campaign_id;
+    private UUID device_id;
+
+    public CampaignDevice(UUID campaign_device_id, UUID campaign_id, UUID device_id)
+    {
+        this.campaign_device_id = campaign_device_id;
+        this.campaign_id = campaign_id;
+        this.device_id = device_id;
+    }
+
+    public UUID getCampaign_device_id()
+    {
+        return campaign_device_id;
+    }
+
+    public void setCampaign_device_id(UUID campaign_device_id)
+    {
+        this.campaign_device_id = campaign_device_id;
+    }
+
+    public UUID getCampaign_id()
+    {
+        return campaign_id;
+    }
+
+    public void setCampaign_id(UUID campaign_id)
+    {
+        this.campaign_id = campaign_id;
+    }
+
+    public UUID getDevice_id()
+    {
+        return device_id;
+    }
+
+    public void setDevice_id(UUID device_id)
+    {
+        this.device_id = device_id;
+    }
+
+}
